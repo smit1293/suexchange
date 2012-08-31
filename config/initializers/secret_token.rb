9b1b04e5dfbef62888f57c9c747e9a66ae2f7af5
SKP <smit1293@gmail.com>
2012-08-31 09:47:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suexchange::Application.config.secret_token = '4e6d9d6f1caf76881a018fee7abee6c2925b19b26013f2360f1fe9d54a6c6f4c355e0f45c6c38e62b3a6f4aed007dd9287ec42ff7be5ece642392c694503678e'
